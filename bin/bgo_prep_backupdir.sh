e39af44baa5d00d40779e78242a6f995215cae18
Andrew Davis <addavis@gmail.com>
2025-08-29 12:39:37 -0400

    - unify variable names with other scripts - coding style

--- bin/bgo_prep_backupdir.sh
+++ bin/bgo_prep_backupdir.sh
@@ -6,26 +6,22 @@
 # Part of the borg-go project, see: https://github.com/AndrewDDavis/borg_go
 
 # Configure some common variables, shell options, and functions
-set -eE
-
-src_dir=$(python3 -c "import os; print(os.path.dirname(os.path.realpath('${BASH_SOURCE[0]}')))")
-source "$src_dir/bgo_functions.sh"
+src_dir=$( command python3 -c "import os; print(os.path.dirname(os.path.realpath('${BASH_SOURCE[0]}')))" )
+builtin source "$src_dir/bgo_env_setup.sh"
 
 # store these file and application lists in logged-in user's home
-# - from bgo_functions: defines lognm, lognm_group, lognm_home
-def_lognm
-
+# - lognm variables set in bgo_env_setup
 bakdir="$lognm_home/.local/backups"
-[[ ! -d "$bakdir" ]] \
-    && /bin/mkdir -p "$bakdir"
+[[ -d "$bakdir" ]] \
+    || /bin/mkdir -p "$bakdir"
 
 
 # Create a list of what's in /usr/local (nullglob is set)
-echo $'/usr/local/\n'           >  "$bakdir"/usr-local-list.txt
-/bin/ls -l /usr/local/          >> "$bakdir"/usr-local-list.txt
-echo $'\n\n/usr/local/*\n'      >> "$bakdir"/usr-local-list.txt
-/bin/ls -l /usr/local/*         >> "$bakdir"/usr-local-list.txt
-/bin/ls -l /usr/local/share/man >> "$bakdir"/usr-local-list.txt
+printf '%s\n\n' '/usr/local/'       >  "$bakdir"/usr-local-list.txt
+/bin/ls -l /usr/local/              >> "$bakdir"/usr-local-list.txt
+printf '\n\n%s\n\n' '/usr/local/*'  >> "$bakdir"/usr-local-list.txt
+/bin/ls -l /usr/local/*             >> "$bakdir"/usr-local-list.txt
+/bin/ls -l /usr/local/share/man     >> "$bakdir"/usr-local-list.txt
 
 # - also show it in tree form
 command tree -a /usr/local > "$bakdir"/usr-local-tree.txt
@@ -50,38 +46,38 @@ fi
 # List packages installed through npm, pip, pipx, and flatpak
 # - you may need to pass PIPX_GLOBAL_HOME if set
 
-if npm_pth=$( builtin type -P npm )
+if npm_cmd=$( builtin type -P npm )
 then
-    "$npm_pth" --version > "${bakdir}/npm-list-g.txt"
-    "$npm_pth" list -g --depth=0 >> "${bakdir}/npm-list-g.txt"
+    "$npm_cmd" --version > "${bakdir}/npm-list-g.txt"
+    "$npm_cmd" list -g --depth=0 >> "${bakdir}/npm-list-g.txt"
 fi
 
-if pip_pth=$( builtin type -P pip )
+if pip_cmd=$( builtin type -P pip )
 then
-    "$pip_pth" --version > "${bakdir}/pip-list.txt"
-    "$pip_pth" list >> "${bakdir}/pip-list.txt"
+    "$pip_cmd" --version > "${bakdir}/pip-list.txt"
+    "$pip_cmd" list >> "${bakdir}/pip-list.txt"
 fi
 
-if pipx_pth=$( builtin type -P pipx )
+if pipx_cmd=$( builtin type -P pipx )
 then
     if [[ ! -v PIPX_GLOBAL_HOME && -d /usr/local/opt/pipx ]]
     then
         export PIPX_GLOBAL_HOME=/usr/local/opt/pipx
     fi
 
-    "$pipx_pth" --version > "${bakdir}/pipx-list-global.txt"
-    "$pipx_pth" list --global >> "${bakdir}/pipx-list-global.txt"
+    "$pipx_cmd" --version > "${bakdir}/pipx-list-global.txt"
+    "$pipx_cmd" list --global >> "${bakdir}/pipx-list-global.txt"
 fi
 
-if fp_pth=$( builtin type -P flatpak )
+if fp_cmd=$( builtin type -P flatpak )
 then
-    "$fp_pth" --version > "${bakdir}/flatpak-list-app.txt"
-    "$fp_pth" list --app >> "${bakdir}/flatpak-list-app.txt"
+    "$fp_cmd" --version > "${bakdir}/flatpak-list-app.txt"
+    "$fp_cmd" list --app >> "${bakdir}/flatpak-list-app.txt"
 fi
 
 # Back up "Session Buddy" backup files of the Chrome state, if any
 # - probably in ~/Downloads
-command find "$lognm_home"/Downloads/ -maxdepth 2 \
+"$find_cmd" "$lognm_home"/Downloads/ -maxdepth 2 \
     -name 'session_buddy_backup*.json' \
     -exec /bin/mv -f {} "$bakdir" \;
 
@@ -99,10 +95,8 @@ then
 fi
 
 
-### OS dependent files
-
-def_mach_id  # from bgo_functions: defines mach_name and mach_os
-
+# OS dependent files
+# - mach_name and mach_os come from bgo_env_setup: defines mach_name and mach_os
 if [[ $mach_os == linux ]]
 then
     # create a list of installed packages from dpkg
@@ -110,21 +104,25 @@ then
     #   http://www.webupd8.org/2010/03/2-ways-of-reinstalling-all-of-your.html
     # - Note, /var/backups contains backups of the dpkg-status file, but that doesn't
     #   appear to indicate manual vs automatic installations.
-
-    dpkg --get-selections > "$bakdir"/dpkg-installed-applications.txt
+    command dpkg --get-selections > "$bakdir"/dpkg-installed-applications.txt
 
     # specifically note manually installed packages
-    apt-mark showmanual > "$bakdir"/apt-manual-packages.txt
+    command apt-mark showmanual > "$bakdir"/apt-manual-packages.txt
 
     # To restore the dpkg list, use:
     #  sudo dpkg --set-selections < dpkg-installed-applications.txt
     #  sudo apt-get -y update
     #  sudo apt-get dselect-upgrade
 
-    if [[ -n $( command -v dconf ) ]]
+    if dconf_cmd=$( builtin type -P dconf )
     then
-        # back up gsettings database if available
-        dconf dump / > "$bakdir"/dconf-dump_backup.dump
+        # back up user gsettings database if available
+        if (( $( command id -u ) == 0 ))
+        then
+            sudo -u "$lognm" "$dconf_cmd" dump / > "$bakdir"/dconf-dump_backup.dump
+        else
+            "$dconf_cmd" dump / > "$bakdir"/dconf-dump_backup.dump
+        fi
     fi
 
 elif [[ $mach_os == macos ]]
@@ -132,9 +130,10 @@ then
     # create a list of installed applications (nullglob is set)
     /bin/ls -l /Applications/ /Users/*/Applications/ > "$bakdir"/applications-list.txt
 else
-    raise 2 "Undefined mach_os: $mach_os"
+    err_msg -d 2 "Undefined mach_os: $mach_os"
+    exit
 fi
 
 # chown these files to user
 # - could also use ACLs with setfacl here, to preserve orig ownership
-chown -R "$lognm":"$lognm_group" "$bakdir"
+"$chown_cmd" -R "$lognm":"$lognm_group" "$bakdir"
